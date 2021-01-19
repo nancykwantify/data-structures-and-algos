2b78679624f321cb0659af3f7e247dc5ab6aeae2
Nancy <nancykwantify@gmail.com>
2021-01-19 00:00:00 -0500

    maxsumarray

--- Problem-Solving-Patterns/Multiple-Pointers/maxSubarraySum.js
+++ Problem-Solving-Patterns/Multiple-Pointers/maxSubarraySum.js
@@ -1,7 +1,27 @@
 /*
 Takes array of integers and a number called n. The function should calculate maximum sum of n consecutive elements in the array.
 */
+/*
+make max sum variable
+make temp sum variable
+check arr length if longer than num // return null
+iterate thru num
+max sum + arr[i]
 
-function maxSubarraySum(arr, n) {}
+ */
+
+function maxSubarraySum(arr, n) {
+  let sum = 0;
+}
 
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+
+/* add one, subtract one
+L  R
+ [1, 2, 5, 2, 8, 1, 5] -----> 10
+
+         L        R
+ [1, 2, 5, 2, 8, 1, 5] -----> 17
+
+*/
