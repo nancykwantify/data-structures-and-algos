b802c963f9941e40b5f567045f8f9fca083d3652
Nancy <nancykwantify@gmail.com>
2021-01-23 23:19:26 -0500

    isSubsequence

---
+++ Problem-Solving-Patterns/Multiple-Pointers/isSubsequence.js
@@ -0,0 +1,19 @@
+/**
+ * Takes two strings and checks whether the characters in the first string form a subsequence of the characters in the second string.
+ *
+ * In other words, the function should check whether the characters int eh first string appear somewhere in the second string, WITHOUT THEIR ORDER CHANGING.
+ *
+ * isSubsequence('hello", "hello world") // true
+ * isSubsequence("sing", "sting") // true
+ * isSubsequence("abc", 'acb') // false
+ *
+ *
+ *
+ */
+
+function isSubsequence(str1, str2) {}
+
+// TEST
+isSubsequence("hello", "hello world"); // true
+isSubsequence("sing", "sting"); // true
+isSubsequence("abc", "acb"); // false
