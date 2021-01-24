12fd90db6517b0901ba495c5307b544e7ffae755
Nancy <nancykwantify@gmail.com>
2021-01-24 15:40:38 -0500

    isSubsequence iterative

--- Problem-Solving-Patterns/Multiple-Pointers/isSubsequence.js
+++ Problem-Solving-Patterns/Multiple-Pointers/isSubsequence.js
@@ -8,12 +8,30 @@
  * isSubsequence("abc", 'acb') // false
  *
  *
+ * set leftIdx pointer
+ * set rightIdx pointer
+ * iterate thru str2
+ *   while iteration is less than str2 length
+ *      if str2[rightIdx] === str1[leftIdx] increment
+ *      if (leftIdx === str1.length) return true
+ *
+ * otherwise, return false
  *
  */
 
-function isSubsequence(str1, str2) {}
+function isSubsequence(str1, str2) {
+  let left = 0;
+  let right = 0;
+  if (!str1) return true;
+  while (right < str2.length) {
+    if (str2[right] === str1[left]) left++;
+    if (left === str1.length) return true;
+    right++;
+  }
+  return false;
+}
 
 // TEST
-isSubsequence("hello", "hello world"); // true
-isSubsequence("sing", "sting"); // true
-isSubsequence("abc", "acb"); // false
+// console.log(isSubsequence("hello", "hello world")); // true
+// console.log(isSubsequence("sing", "sting")); // true
+console.log(isSubsequence("abc", "acb")); // false
---
+++ Problem-Solving-Patterns/Sliding-Window/maxSubarraySum.js
@@ -0,0 +1,10 @@
+/**
+ * Given an array of integers and a number, white a fn which finds maximum sum of a subarray with the length of the number passed to the function.
+ * Note a subarray must consist of consecutive elements from the original array in the first
+ *
+ * console.log(maxSubarraySum([100,200,300,400], 2)) // 700
+ */
+
+console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)); // 39
+console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)); // 5
