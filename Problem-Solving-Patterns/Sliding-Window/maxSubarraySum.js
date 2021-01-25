213f6b0b04c3adc48e327e5124afcedceff6f956
Nancy <nancykwantify@gmail.com>
2021-01-25 16:15:20 -0500

    maxSubarraySum

--- Problem-Solving-Patterns/Sliding-Window/maxSubarraySum.js
+++ Problem-Solving-Patterns/Sliding-Window/maxSubarraySum.js
@@ -5,28 +5,59 @@
  * console.log(maxSubarraySum([100,200,300,400], 2)) // 700
  *
  *  L
- * [100,200,300,400]
- *       R
+ * [100,200,300,400] find largest sum
+ *            R
+ * left pointer at 0
+ * right pointer at 0 + num
+ *
+ * let largestSum = 0
+ * iterate thru arr (window)
+ *  newSum = largestSum += arr[i]
+ * if newSum > largestSum then
+ * largest sum = newSum
+ *
+ *
+ * increment Right
+ * increment Left
+ *
+ * return largestSum
+ *
+ *
+ *
+ *
  */
 
-function maxSubarraySum(arr, num) {}
-
-console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)); // 39
-console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
-console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)); // 5
-
-// SLIDING WINDOW
 function maxSubarraySum(arr, num) {
   if (arr.length < num) return null;
 
-  let total = 0;
+  let maxSum = 0;
   for (let i = 0; i < num; i++) {
-    total += arr[i];
+    maxSum += arr[i];
   }
-  let currentTotal = total;
+  let newSum = maxSum;
   for (let i = num; i < arr.length; i++) {
-    currentTotal += arr[i] - arr[i - num];
-    total = Math.max(total, currentTotal);
+    newSum += arr[i] - arr[i - num];
+    maxSum = Math.max(newSum, maxSum);
   }
-  return total;
+  return maxSum;
 }
+
+// console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)); // 39
+console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+// console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)); // 5
+
+// SLIDING WINDOW
+// function maxSubarraySum(arr, num) {
+//   if (arr.length < num) return null;
+
+//   let total = 0;
+//   for (let i = 0; i < num; i++) {
+//     total += arr[i];
+//   }
+//   let currentTotal = total;
+//   for (let i = num; i < arr.length; i++) {
+//     currentTotal += arr[i] - arr[i - num];
+//     total = Math.max(total, currentTotal);
+//   }
+//   return total;
+// }
