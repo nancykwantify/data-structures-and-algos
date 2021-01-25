7a34536deed97cc6893017972ebcdfe25bfb90e7
Nancy <nancykwantify@gmail.com>
2021-01-25 17:39:39 -0500

    linearSearch

---
+++ Searching Algorithms/linearSearch.js
@@ -0,0 +1,11 @@
+/**
+ * Write a fn which accepts an array and val,
+ * and returns the index at which the value exists
+ * if not exist, return -1
+ *
+ * linearSearch([10,15,20,25,30], 15)
+ *
+ */
+
+console.log(linearSearch([10, 15, 20, 25, 30], 15)); // 1
+console.log(linearSearch([100], 100)); // 0
