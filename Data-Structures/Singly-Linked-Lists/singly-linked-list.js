fa62e33598d9d74d07f0126455161f80771fcbb5
Nancy <nancykwantify@gmail.com>
2021-03-02 23:53:38 -0500

    singly linked list remove and reverse

--- Data-Structures/Singly-Linked-Lists/singly-linked-list.js
+++ Data-Structures/Singly-Linked-Lists/singly-linked-list.js
@@ -95,6 +95,35 @@ class SinglyLinkedList {
     this.length++;
     return true;
   }
+  remove(idx) {
+    if (idx < 0 || idx > this.length) return undefined;
+    if (idx === this.length - 1) {
+      return this.pop();
+    }
+    if (idx === 0) {
+      return this.shift();
+    }
+    let previousNode = this.get(idx - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null; // start
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
